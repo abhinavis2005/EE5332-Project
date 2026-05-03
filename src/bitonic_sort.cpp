2b5a484ac6195f264a6fe09fa09d30de577f098e
Abhinav I S <abhinavis2005@gmail.com>
2026-05-03 14:55:33 +0530

    Add synth reports

--- src/bitonic_sort.cpp
+++ src/bitonic_sort.cpp
@@ -5,6 +5,7 @@ void bitonic_sort(data_t arr[N]) {
     for (int k = 2; k <= N; k *= 2) {
         for (int j = k / 2; j > 0; j /= 2) {
             for (int i = 0; i < N; i++) {
+#pragma HLS UNROLL
                 int l = i ^ j;
                 if (l > i) {
                     int dir = (i & k) == 0;
